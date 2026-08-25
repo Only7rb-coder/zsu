531cb662c18ce9d7417f6bf2231ffcb0692a642f
Only7rb-coder <1.89421348e+08+Only7rb-coder@users.noreply.github.com>
2026-08-25 11:32:10 +0000

    Fix contributor icon imports

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Home.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Home.kt
@@ -29,7 +29,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bolt
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.automirrored.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
